e46c6c0c167a3fa68c271c3bd2fa212bc031323f
Neo Sebanze <neo@bidvestalice.com>
2022-05-14 12:47:50 +0200

    Display currency values

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+
+from flask import Flask, flash, request, redirect, url_for, render_template
 from flask import Flask
+import json
 app = Flask(__name__)
+
+#GET DATA FROM JSON
+#def get_rankedcoins():
+file= open('back_end/sample.json')
+data = json.load(file)
+
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+     return render_template('index.html',data=data)
+
+
